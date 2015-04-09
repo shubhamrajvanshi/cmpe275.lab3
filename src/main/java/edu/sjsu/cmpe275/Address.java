228deb1be8280a68a68051a4bb15f48259eeb80a
shubhamrajvanshi <shubhamrajvanshi9@gmail.com>
2015-04-08 23:19:47 -0700

    Lab 3 base

---
+++ src/main/java/edu/sjsu/cmpe275/Address.java
@@ -0,0 +1,57 @@
+package edu.sjsu.cmpe275;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the zip
+	 */
+	public String getZip() {
+		return zip;
+	}
+	/**
+	 * @param zip the zip to set
+	 */
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+    
+}
